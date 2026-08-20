bacb123a74b5f0253ee078e687563caf58885ee5
diaphon <diaphon@users.noreply.github.com>
2026-08-20 09:41:45 +0200

    Add files via upload

---
+++ Daten-abrufen-BF00019971.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import requests, json
+
+BASE_URL = "https://dataservice.dla-marbach.de/v1/records"
+params = {
+    "q": "collection_id_mv:BF00019971", # A:Aichinger, Ilse
+    "format": "json",
+    # "size": "10",
+}
+
+response = requests.get(BASE_URL, params=params)
+print(response.url)
+response.raise_for_status()
+
+data = response.json()
+
+with open("BF00019971.json", 'w') as fh:
+    json.dump(data, fh, indent=2)
